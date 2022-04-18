a5284749b36ccf6beedb5c72ca11ac607f257b27
Jens Vanhooydonck <jensvanhooydonck@gmail.com>
2022-04-18 08:58:42 +0000

    [WIP]

--- tm_driver/src/tm_robot_state.cpp
+++ tm_driver/src/tm_robot_state.cpp
@@ -135,9 +135,9 @@ TmRobotState::~TmRobotState()
 
 void TmRobotState::set_joint_states(const std::vector<double> &pos, const std::vector<double> &vel, const std::vector<double> &tor)
 {
-	// joint_angle() = pos;
-	// joint_speed() = vel;
-	// joint_torque() = tor;
+	joint_angle() = pos;
+	joint_speed() = vel;
+	joint_torque() = tor;
 
 	tmRobotStateDataToPublish = multiThreadCache.get_catch_data();
 
@@ -147,6 +147,12 @@ void TmRobotState::set_joint_states(const std::vector<double> &pos, const std::v
 	    tmRobotStateDataToPublish.joint_torque[i] = tor[i];
 	}
 
+	
+// 	for (size_t i = 0; i < 6; ++i) {
+// 		tmRobotStateDataFromEthernet.joint_angle[i] = pos[i] * (180.0 / M_PI);
+// 		tmRobotStateDataFromEthernet.joint_speed[i] = vel[i];
+// 		tmRobotStateDataFromEthernet.joint_torque[i] = tor[i];
+// 	}
 	multiThreadCache.set_catch_data(tmRobotStateDataToPublish);
 }
 
